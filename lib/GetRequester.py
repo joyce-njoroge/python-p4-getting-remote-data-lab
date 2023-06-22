42adc22db8ebb79c138c31076ecc78651cc127b8
Joyce Njoroge <joycewambuku33@gmail.com>
2023-06-22 13:19:00 +0300

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import unittest
 
 class GetRequester:
 
@@ -7,7 +8,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()
+            return response.content  # Return response as byte string
+        except requests.exceptions.RequestException as err:
+            print(f'Error occurred during request: {err}')
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            return json.loads(response_body)
+        else:
+            return None
+
+
+
+
